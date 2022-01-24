ae9e8fab5d17c2c59b57ae45df01b6191d3e610e
waseem <waseempasha@digitalgreen.org>
2022-01-24 09:48:52 +0530

    Small changes

--- helpers/config.py
+++ helpers/config.py
@@ -134,6 +134,8 @@ class Config:
             for key in cert_files.keys():
                 cert_file = os.path.join(self.__dict['base_dir'], 'docker', 'config', key)
                 lets_encrypt_file = os.path.join(lets_encrypt_dir, cert_files[key])
+                print(cert_file)
+                print(lets_encrypt_file)
                 subprocess.run(["sudo", "cp", lets_encrypt_file, cert_file], shell=True)
                 subprocess.run(['sudo', 'chown', '${USER}:${USER}', cert_file], shell=True)
 

f44e44dcc4a977dba7faf382aac0c26941fc684d
waseem <waseempasha@digitalgreen.org>
2022-01-24 09:00:45 +0530

    Server changes

--- helpers/config.py
+++ helpers/config.py
@@ -112,34 +112,34 @@ class Config:
         return current_path
     
     def generate_ssl_certificate(self):
-        try:
-            lets_encrypt_dir = os.path.join(Config.LETS_ENCRYPT_BASE_URL, self.__dict['public_domain'])
-            cert_files = {
-                'public.crt': 'fullchain.pem',
-                'private.key': 'privkey.pem'
-            }
+        
+        lets_encrypt_dir = os.path.join(Config.LETS_ENCRYPT_BASE_URL, self.__dict['public_domain'])
+        cert_files = {
+            'public.crt': 'fullchain.pem',
+            'private.key': 'privkey.pem'
+        }
 
-            # 1. Install certbot.
-            subprocess.run("sudo apt-get install certbot", shell=True)
+        # 1. Install certbot.
+        subprocess.run("sudo apt-get install certbot", shell=True)
 
-            # 2. Provide Information..
-            email = CLI.colored_input(message='Enter your email for TLS/SSL certificate renewal: ')
-            certbot_command = ['sudo', 'certbot', 'certonly', '--standalone', '-d', self.__dict['public_domain'], '--agree-tos',
-                            '--non-interactive', '-m', email]
-            subprocess.run(certbot_command, shell=True)
-            print("email registered..")
-            # 3. Copy Keys to config folder and change permissions.
-            for key in cert_files.keys():
-                cert_file = os.path.join(self.get_env_files_path, 'config', key)
-                lets_encrypt_file = os.path.join(lets_encrypt_dir, cert_files[key])
-                print(cert_file)
-                print(lets_encrypt_file)
-                subprocess.run(["sudo", "cp", lets_encrypt_file, cert_file], shell=True)
-                subprocess.run(['sudo', 'chown', '${USER}:${USER}', cert_file], shell=True)
+        # 2. Provide Information..
+        email = CLI.colored_input(message='Enter your email for TLS/SSL certificate renewal: ')
+        certbot_command = ['sudo', 'certbot', 'certonly', '--standalone', '-d', self.__dict['public_domain'], '--agree-tos',
+                        '--non-interactive', '-m', email]
+        subprocess.run(certbot_command, shell=True)
 
-        except Exception as err:
-            CLI.colored_print(message='Error Installing SSL Certificate', color=CLI.COLOR_ERROR)
-            sys.exit(1)
+        # 3. Copy Keys to config folder and change permissions.
+        for key in cert_files.keys():
+            cert_file = os.path.join(self.get_env_files_path, 'config', key)
+            lets_encrypt_file = os.path.join(lets_encrypt_dir, cert_files[key])
+            print(cert_file)
+            print(lets_encrypt_file)
+            subprocess.run(["sudo", "cp", lets_encrypt_file, cert_file], shell=True)
+            subprocess.run(['sudo', 'chown', '${USER}:${USER}', cert_file], shell=True)
+
+        # except Exception as err:
+        #     CLI.colored_print(message='Error Installing SSL Certificate', color=CLI.COLOR_ERROR)
+        #     sys.exit(1)
     
     def __questions_steward_backend_usm(self):
         CLI.framed_print(message=('Step 2:'
